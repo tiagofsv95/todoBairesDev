1bd893338ecef694a6d1a5635f63ad6e629b7fd8
unknown <tiagofsv95@gmail.com>
2021-06-03 09:04:40 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+  </Switch>
+);
